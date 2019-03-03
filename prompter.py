07d7c7bcdb81e1453a634cd740ab2e0e80473c36
Jesse Zhang <macisasandwich@gmail.com>
2019-03-02 21:33:38 -0600

    framework setup

--- prompter.py
+++ prompter.py
@@ -3,22 +3,44 @@ from cmd import Cmd
 class DSTSPrompt(Cmd):
     prompt = 'DSTrueSKill> '
     intro = "Welcome! Type ? to list commands"
- 
-    def do_exit(self, inp):
-        print("Bye")
+
+    def do_add(self, arg):
+        # adds player
+        pass
+
+    def do_list(self, arg):
+        # lists existing players in the environment
+        pass 
+
+    def do_match(self, arg):
+        # iteration 1: requires winner and loser predecided and enter in order
+        # just updates MMR 
+        pass
+
+    def do_exit(self, arg):
+        # writes out environment and exit
+
         return True
-    
-    def help_exit(self):
-        print('exit the application. Shorthand: x q Ctrl-D.')
- 
-    def do_add(self, inp):
-        print("adding '{}'".format(inp))
- 
+
     def help_add(self):
-        print("Add a new entry to the system.")
- 
-    def default(self, inp):
-        if inp == 'x' or inp == 'q':
-            return self.do_exit(inp)
- 
-        print("Default: {}".format(inp))
+        pass 
+
+    def help_list(self):
+        pass 
+
+    def help_match(self):
+        pass
+
+    def help_exit(self):
+        pass
+
+    def default(self, arg):
+        if arg == 'q':
+            return self.do_exit(arg)
+
+        print("Got '{0}'. Invalid input or not implemented.".format(arg))
+
+    def __init__(self):
+        super(DSTSPrompt, self).__init__()
+
+        # create/load environment here
